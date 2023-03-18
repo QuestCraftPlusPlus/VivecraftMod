e5bbcbbb78204b63544ffd3c1cffd40dd49de057
The Judge <colepatrickroberts@gmail.com>
2023-03-17 17:35:42 -0700

    Fix merge issues

--- common/src/main/java/org/vivecraft/modCompatMixin/sodiumMixin/RenderSectionManagerVRMixin.java
+++ common/src/main/java/org/vivecraft/modCompatMixin/sodiumMixin/RenderSectionManagerVRMixin.java
@@ -1,7 +1,7 @@
 package org.vivecraft.modCompatMixin.sodiumMixin;
 
+import me.jellysquid.mods.sodium.client.render.chunk.ChunkRenderList;
 import me.jellysquid.mods.sodium.client.render.chunk.RenderSectionManager;
-import me.jellysquid.mods.sodium.client.render.chunk.lists.ChunkRenderList;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
